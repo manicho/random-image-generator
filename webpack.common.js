75cc89467f5453a3e69a0878ce933b8e155d66f6
manicho <manicho@gmail.com>
2021-02-28 23:22:39 -0300

    added .env support

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,18 @@
+const dotenv = require('dotenv');
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { DefinePlugin } = require('webpack')
+
+// call dotenv and it will return an Object with a parsed key 
+const env = dotenv.config().parsed;
+
+// reduce it to a nice object, the same as before
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 
 module.exports = {
   entry: {
@@ -49,6 +60,7 @@ module.exports = {
     extensions: ["*", ".js", ".jsx"],
   },
   plugins: [
+    new DefinePlugin(envKeys),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
